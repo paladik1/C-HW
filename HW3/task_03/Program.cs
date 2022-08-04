77cc46d46b181e7910fa5ceb96d5662f07a6b8ca
Evgeny Makhnutin <evgenymakhnutin@gmail.com>
2022-08-04 21:51:04 +0300

    HW 3

--- HW3/task_03/Program.cs
+++ HW3/task_03/Program.cs
@@ -3,9 +3,29 @@
 //5 -> 1, 8, 27, 64, 125
 
 Console.WriteLine("Введите ваше число");
-int count = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите необходимую степень");
-int deg = int.Parse(Console.ReadLine());
-int value = 3;
-for (int power = 0; count <= 32; count++)
-    Console.WriteLine($"{value}^{power} = {(long)Math.Pow(value, power):N0} (0x{(long)Math.Pow(value, power):X})");
+int num = int.Parse(Console.ReadLine());
+int[] array = new int[num + 1];
+Deg(array);
+PrintArry(array);
+
+void Deg(int[] num)
+{
+    int count = 0;
+    int length = num.Length;
+    while (count < length)
+    {
+        num[count] = Convert.ToInt32(Math.Pow(count, 3));
+        count++;
+    }
+}
+
+void PrintArry(int[] coll)
+{
+    int count = coll.Length;
+    int index = 1;
+    while (index < count)
+    {
+        Console.Write(coll[index] + " ");
+        index++;
+    }
+}
